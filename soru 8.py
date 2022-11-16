e0f35f25244d78483f0680b5fc40a31c9fd309fd
Boran Biçer <114919885+boranbicer@users.noreply.github.com>
2022-11-16 19:28:25 +0300

    Update soru 8.py

--- soru 8.py
+++ soru 8.py
@@ -1,3 +1,5 @@
+a=[]
 for i in range(100,1000):
-    if i%2==0 and int(str(i)[0])!=int(str(i)[1]) and int(str(i)[0])!=int(str(i)[2]) and int(str(i)[1])!=int(str(i)[2]):
-        print(i)
+    if i%2==0 and int(str(i)[0])==int(str(i)[1]) or int(str(i)[0])==int(str(i)[2]) or int(str(i)[1])==int(str(i)[2]) or int(str(i)[0])==int(str(i)[1])==int(str(i)[2]) :
+        a.append(i)
+print(len(a))
