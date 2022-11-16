2394d5a69bc3830160954b2bb6f0dc5b077b959d
Boran Biçer <114919885+boranbicer@users.noreply.github.com>
2022-11-16 21:42:59 +0300

    Update soru 14.py

--- soru 14.py
+++ soru 14.py
@@ -1,4 +1,11 @@
-for i in range(121212):
-    for j in range(12122):
-        if i*j==121212:
-            print((abs(int(i-j))))
+
+a = []
+for i in range(1,121212):
+    for j in range(1,121212):
+        if i*j == 121212:
+            a.append((abs(i-j),i,j))
+
+for k in a:
+    if k==min(a):
+        print(k[1],k[2])
+        break
